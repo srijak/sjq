d70259607f95afafef6e1dff4b77ccabc58dcda1
srijak <srijak@gmail.com>
2010-10-25 14:14:55 -0400

    added CPUT message. Still need to implement it tho :)

--- src/main/scala/sjq/Codec/SjqCodec.scala
+++ src/main/scala/sjq/Codec/SjqCodec.scala
@@ -5,9 +5,11 @@ import org.apache.mina.core.session.{ IdleStatus, IoSession }
 import org.apache.mina.filter.codec._
 import net.lag.naggati._
 import net.lag.naggati.Steps._
+import java.net.{ URL, MalformedURLException }
 
 abstract class Request
 case class PUT(q: String, data: String) extends Request
+case class CPUT(q: String, callback_url: URL, data: String) extends Request
 case class GET(q: String) extends Request
 
 case class Response(data: IoBuffer)
@@ -32,14 +34,29 @@ object SjqCodec {
         throw new ProtocolError("Malformed queue name. Must match: [A-Za-z0-9_]{2,30}")
       }
     }
+    def parseURL(cb_url: String): URL = {
+      try {
+        new URL(cb_url)
+      } catch {
+        case me: MalformedURLException => throw new ProtocolError("Malformed callback URL: " + cb_url)
+        case _ => throw new ProtocolError("Could not create URL from " + cb_url)
+      }
+    }
 
     val msg = new String(buffer, 0, buffer.length - EOM_Bytes.length)
       .split("\n", 2)
     val cmd = msg(0).split(' ').toList.map(e => e.trim)
 
     cmd match {
-      case "PUT" :: q :: Nil => assertValidQueueName(q); state.out.write(PUT(q, msg(1))); End
-      case "GET" :: q :: Nil => assertValidQueueName(q); state.out.write(GET(q)); End
+      case "PUT" :: q :: Nil =>
+        assertValidQueueName(q)
+        state.out.write(PUT(q, msg(1))); End
+      case "CPUT" :: q :: cb_url :: Nil =>
+        assertValidQueueName(q)
+        state.out.write(CPUT(q, parseURL(cb_url), msg(1))); End
+      case "GET" :: q :: Nil =>
+        assertValidQueueName(q)
+        state.out.write(GET(q)); End
       case _ => throw new ProtocolError("Malformed request:" + cmd.toString)
     }
   })
--- src/test/scala/SjqCodec_DecoderSpec.scala
+++ src/test/scala/SjqCodec_DecoderSpec.scala
@@ -21,7 +21,7 @@ object SjqCodecDecoder_Spec extends Specification {
   }
 
   def quickDecode(s: String): Unit = {
-    SjqCodec.decoder.decode(fakeSession, IoBuffer.wrap(s.getBytes("UTF-8")), fakeDecoderOutput)
+    SjqCodec.decoder.decode(fakeSession, IoBuffer.wrap((s + SjqCodec.EOM).getBytes("UTF-8")), fakeDecoderOutput)
   }
 
   "SjqCodec Decoder" should {
@@ -30,44 +30,62 @@ object SjqCodecDecoder_Spec extends Specification {
     }
 
     "parse PUT" in {
-      quickDecode("PUT qname\n{\"key\": \"value\"}" + SjqCodec.EOM)
+      quickDecode("PUT qname\n{\"key\": \"value\"}")
       written(0) must haveClass[PUT]
       written(0).asInstanceOf[PUT].data mustEqual "{\"key\": \"value\"}"
       written(0).asInstanceOf[PUT].q mustEqual "qname"
     }
 
     "parse PUT, no queue name throws ProtocolError" in {
-      quickDecode("PUT \n{\"key\": \"value\"}" + SjqCodec.EOM) must throwA[ProtocolError]
+      quickDecode("PUT \n{\"key\": \"value\"}") must throwA[ProtocolError]
     }
 
     "parse PUT, queue name length less than 2 throws ProtocolError" in {
-      quickDecode("PUT q\n{\"key\": \"value\"}" + SjqCodec.EOM) must throwA[ProtocolError]
+      quickDecode("PUT q\n{\"key\": \"value\"}") must throwA[ProtocolError]
     }
 
     "parse PUT, queue name length greater than 30 throws ProtocolError" in {
-      quickDecode("PUT queue_name_that_is_longer_than_30_characters\n{\"key\": \"value\"}" + SjqCodec.EOM) must throwA[ProtocolError]
+      quickDecode("PUT queue_name_that_is_longer_than_30_characters\n{\"key\": \"value\"}") must throwA[ProtocolError]
     }
 
     "parse GET" in {
-      quickDecode("GET qname" + SjqCodec.EOM)
+      quickDecode("GET qname")
       written.size mustEqual 1
       written(0) must haveClass[GET]
       written(0).asInstanceOf[GET].q mustEqual "qname"
     }
+
     "parse GET, no queue name throws ProtocolError" in {
-      quickDecode("GET" + SjqCodec.EOM) must throwA[ProtocolError]
+      quickDecode("GET") must throwA[ProtocolError]
     }
 
     "parse GET, queue name length less than 2 throws ProtocolError" in {
-      quickDecode("GET q" + SjqCodec.EOM) must throwA[ProtocolError]
+      quickDecode("GET q") must throwA[ProtocolError]
     }
 
     "parse GET, queue name length greater than 30 throws ProtocolError" in {
-      quickDecode("GET queue_name_that_is_longer_than_30_characters" + SjqCodec.EOM) must throwA[ProtocolError]
+      quickDecode("GET queue_name_that_is_longer_than_30_characters") must throwA[ProtocolError]
     }
 
     "parse invalid command" in {
-      quickDecode("KAPUT\n" + SjqCodec.EOM) must throwA[ProtocolError]
+      quickDecode("KAPUT\n") must throwA[ProtocolError]
     }
+
+    "parse CPUT" in {
+      quickDecode("CPUT qname http://google.com\nblah blah blah")
+      written.size mustEqual 1
+      written(0) must haveClass[CPUT]
+      written(0).asInstanceOf[CPUT].q mustEqual "qname"
+      written(0).asInstanceOf[CPUT].callback_url.getHost mustEqual "google.com"
+    }
+
+    "parse CPUT, invalid URL throws ProtocolError" in {
+      quickDecode("CPUT qname google.com\n blah blah blah") must throwA[ProtocolError]
+    }
+
+    "parse CPUT, empty URL throws ProtocolError" in {
+      quickDecode("CPUT qname  \n blah blah blah") must throwA[ProtocolError]
+    }
+
   }
 }
